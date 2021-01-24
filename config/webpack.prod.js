fec0a5030d161110113c17f4b88eefd529d22481
Dimitri Basseguy <dimitri.basseguy@gmail.com>
2021-01-24 23:38:37 +0100

    correction pour le build

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,17 +1,17 @@
 const paths = require('./paths');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const { BundleStatsWebpackPlugin } = require('bundle-stats-webpack-plugin');
 
-module.exports = merge.smart(common, {
+module.exports = merge(common, {
   mode: 'production',
   devtool: false,
   output: {
     path: paths.build,
-    publicPath: './',
+    publicPath: 'https://www.dimitri-basseguy.fr/shadowrun/',
     filename: 'js/[name].[contenthash].js',
   },
   plugins: [
